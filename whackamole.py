99c4d778d8f494926e777314369471013c58f2a9
JordanGraham06 <jordangraham@ufl.edu>
2024-11-12 13:12:50 -0500

    whatever

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -10,15 +10,45 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+
+
+        def draw_grid(screen):
+            for i in range(0,21):
+                pygame.draw.line(screen, (0,0,0), (i*32, 0), (i*32, 512))
+            for i in range(0,17):
+                pygame.draw.line(screen, (0,0,0), (0, i*32), (640, i*32))
+
+        screen.fill("light green")
+        draw_grid(screen)
+        screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
         while running:
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    a, b = event.pos
+                    if a//32 == 0 and b//32 == 0:
+                        screen.fill("light green")
+                        draw_grid(screen)
+                        x = random.randrange(1, 20)
+                        y = random.randrange(1, 16)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(x*32, y*32)))
+                    if a//32 == x and b//32 == y:
+                        screen.fill("light green")
+                        draw_grid(screen)
+                        x = random.randrange(1, 20)
+                        y = random.randrange(1, 16)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(x*32, y*32)))
+
+
+
             pygame.display.flip()
             clock.tick(60)
+
     finally:
         pygame.quit()
+
 
 
 if __name__ == "__main__":
